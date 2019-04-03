fba7c192c8a61a739cca4111c7eb71108dbb735c
田 余 <yutian_terry@foxmail.com>
2019-04-03 17:57:57 +0800

    optimize

---
+++ WorkshopDemo/Assets/Workshop/Test.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Vehicle
+{
+    private float mass;
+    private Vector3 postion;
+    private string wheelInformation;
+
+    public void Move()
+    {
+        //Change position
+    }
+}
