a8b96d8008b5e9feb7b71fdb551185c61c6001b1
田 余 <yutian_terry@foxmail.com>
2019-04-04 11:34:19 +0800

    optimize

---
+++ WorkshopDemo/Assets/WorkshopSamples/Utils/SystemControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Unity.Entities;
+
+public class SystemControl : MonoBehaviour
+{
+    
+    private void Awake()
+    {
+        World.Active.GetExistingManager<YU.ECS.T1.GenerateCubeSystem>().Enabled = false;
+        World.Active.GetExistingManager<YU.ECS.T2.GenerateCubeSystem>().Enabled = false;
+        World.Active.GetExistingManager<YU.ECS.T2.MoveCubeSystem>().Enabled = false;
+        World.Active.GetExistingManager<YU.ECS.T3.GenerateCubeSystem>().Enabled = false;
+        World.Active.GetExistingManager<YU.ECS.T3.MoveCubeSystem>().Enabled = false;
+        World.Active.GetExistingManager<YU.ECS.T4.GenerateCubeSystem>().Enabled = false;
+        World.Active.GetExistingManager<YU.ECS.T4.MoveCubeSystem>().Enabled = false;
+        World.Active.GetExistingManager<YU.ECS.T4.HealthSystem>().Enabled = false;
+    }
+
+}
