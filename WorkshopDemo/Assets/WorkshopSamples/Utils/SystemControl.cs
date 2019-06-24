25681cc0427a183d4aaa0d5a216c504d01c5d449
田 余 <yutian_terry@foxmail.com>
2019-06-24 16:45:44 +0800

    auto switch system

--- WorkshopDemo/Assets/WorkshopSamples/Utils/SystemControl.cs
+++ WorkshopDemo/Assets/WorkshopSamples/Utils/SystemControl.cs
@@ -1,19 +1,46 @@
 ﻿using UnityEngine;
 using Unity.Entities;
-
+using UnityEngine.SceneManagement;
 public class SystemControl : MonoBehaviour
 {
     
     private void Awake()
     {
-        World.Active.GetExistingManager<YU.ECS.T1.GenerateCubeSystem>().Enabled = false;
-        World.Active.GetExistingManager<YU.ECS.T2.GenerateCubeSystem>().Enabled = false;
-        World.Active.GetExistingManager<YU.ECS.T2.MoveCubeSystem>().Enabled = false;
-        World.Active.GetExistingManager<YU.ECS.T3.GenerateCubeSystem>().Enabled = false;
-        World.Active.GetExistingManager<YU.ECS.T3.MoveCubeSystem>().Enabled = false;
-        World.Active.GetExistingManager<YU.ECS.T4.GenerateCubeSystem>().Enabled = false;
-        World.Active.GetExistingManager<YU.ECS.T4.MoveCubeSystem>().Enabled = false;
-        World.Active.GetExistingManager<YU.ECS.T4.HealthSystem>().Enabled = false;
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "Tutorial01":
+                World.Active.GetExistingManager<YU.ECS.T2.GenerateCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T2.MoveCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T3.GenerateCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T3.MoveCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T4.GenerateCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T4.MoveCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T4.HealthSystem>().Enabled = false;
+                break;
+            case "Tutorial02":
+                World.Active.GetExistingManager<YU.ECS.T1.GenerateCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T3.GenerateCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T3.MoveCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T4.GenerateCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T4.MoveCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T4.HealthSystem>().Enabled = false;
+                break;
+            case "Tutorial03":
+                World.Active.GetExistingManager<YU.ECS.T1.GenerateCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T2.GenerateCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T2.MoveCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T4.GenerateCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T4.MoveCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T4.HealthSystem>().Enabled = false;
+                break;
+            case "Tutorial04":
+                World.Active.GetExistingManager<YU.ECS.T1.GenerateCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T2.GenerateCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T2.MoveCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T3.GenerateCubeSystem>().Enabled = false;
+                World.Active.GetExistingManager<YU.ECS.T3.MoveCubeSystem>().Enabled = false;
+                break;
+        }
     }
 
 }
